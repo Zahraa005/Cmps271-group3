937a7315602e7a3a65983628246d002f7c0616d8
hazem-ui <mmi49@mail.aub.edu>
2025-11-09 20:22:06 +0200

    Update User_stats.py
    added xp and level

--- PlayConnect_API/schemas/User_stats.py
+++ PlayConnect_API/schemas/User_stats.py
@@ -6,14 +6,19 @@ __all__ = ["UserStatCreate", "UserStatRead"]
 
 class UserStatCreate(BaseModel):
     user_id: int
+    sport_id: int
     games_played: Optional[int] = 0
     games_hosted: Optional[int] = 0
     attendance_rate: Optional[Decimal] = None
-    sport_id: int
+    xp: Optional[int] = 0
+    level: Optional[int] = 0
+
 
 class UserStatRead(BaseModel):
     user_id: int
+    sport_id: int
     games_played: Optional[int] = 0
     games_hosted: Optional[int] = 0
     attendance_rate: Optional[Decimal] = None
-    sport_id: int
+    xp: Optional[int] = 0
+    level: Optional[int] = 0
